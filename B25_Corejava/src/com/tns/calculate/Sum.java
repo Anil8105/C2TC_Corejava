a7a588f2145963ad7569b795f60a27ceff139829
Anil <Anil@anil>
2022-09-13 09:43:30 +0530

    Package creation and usage

---
+++ B25_Corejava/src/com/tns/calculate/Sum.java
@@ -0,0 +1,12 @@
+package com.tns.calculate;
+
+public class Sum {
+	int a=25;
+	int b=35;
+	public void cal()
+	{
+		int s=a+b;
+		System.out.println("Sum:"+s);
+	}
+
+}
---
+++ B25_Corejava/src/com/tns/progms/scanner.java
@@ -0,0 +1,16 @@
+package com.tns.progms;
+import java.util.*;
+public class scanner {
+
+	public static void main(String[] args) {
+	 Scanner sc=new Scanner(System.in);
+	 String name=sc.nextLine();
+	 char gender=sc.next().charAt(0);
+	 int age=sc.nextInt();
+	 System.out.println("name is"+name);
+	 System.out.println("gender is "+gender);
+	 System.out.println("age is "+age);
+	 
+	}
+
+}
